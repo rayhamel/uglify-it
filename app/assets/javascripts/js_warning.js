70a01955de2cd6d06ace810eacb84b3059a1d413
Ray Hamel <rayghamel@gmail.com>
2015-04-18 23:37:36 -0400

    many improvements to tooltip

--- app/assets/javascripts/js_warning.js
+++ app/assets/javascripts/js_warning.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-    $("#js-warning-link").click(function () {
+    $("#js-warning-link").on("click.showJSWarning", function () {
         $(this).hide();
         $("#js-warning-text").show();
         return false;
     });
-    $("#js-warning-text").click(function () {
+    $("#js-warning-text").on("click.hideJSWarning", function () {
         $(this).hide();
         $("#js-warning-link").show();
         return false;
--- app/assets/javascripts/tooltip.js
+++ app/assets/javascripts/tooltip.js
@@ -1,7 +1,48 @@
-$(document).click(function (e) {
-    $(".tooltip").remove();
-    $("body").append('<span class="tooltip">Test!<span/>');
-    $(".tooltip").css("left", e.pageX + 8);
-    $(".tooltip").css("top", e.pageY - 11);
+$("body").on("click.placeTooltip", function (e) {
+    $(".uglifytooltip").css("display", "inline-block");
+    $(".uglifytooltip").css("left", e.pageX + 8);
+    $(".uglifytooltip").css("top", e.pageY - 11);
     return false;
 });
+
+$(".uglifyminbutton.setmin").on("click.minTooltip", function (e) {
+    e.stopImmediatePropagation();
+    $(".uglifyoverlay").show();
+    $(this).hide();
+    $(".uglifyminbutton.setmax").show();
+    $(".uglifytooltip").css("opacity", "0.46");
+    $(".uglifytooltip").css("filter", "alpha(opacity=46)");
+    return false;
+});
+
+$(".uglifyminbutton.setmax").on("click.maxTooltip", function (e) {
+    e.stopImmediatePropagation();
+    showTooltip();
+    return false;
+});
+
+$(".uglifyclosebutton").on("click.closeTooltip", function (e) {
+    e.stopImmediatePropagation();
+    $(".uglifytooltip").hide();
+    showTooltip();
+    return false;
+});
+
+$(".uglifytooltip").on("click.clickTooltip", function (e) {
+    e.stopImmediatePropagation();
+    showTooltip();
+    return false;
+});
+
+$(".uglifyoverlay").on("click.placeTooltip", function (e) {
+    e.stopImmediatePropagation();
+    return false;
+});
+
+function showTooltip () {
+    $(".uglifyoverlay").hide();
+    $(".uglifyminbutton.setmax").hide();
+    $(".uglifyminbutton.setmin").show();
+    $(".uglifytooltip").css("opacity", "0.92");
+    $(".uglifytooltip").css("filter", "alpha(opacity=92)");
+}
