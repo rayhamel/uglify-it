e8d06befa90ff1940220e8368402f818043d1c53
Ray Hamel <rayghamel@gmail.com>
2015-04-24 08:25:55 -0400

    hopefully fix heroku

--- db/migrate/20150423221225_remove_user_id_from_webpages.rb
+++ db/migrate/20150423221225_remove_user_id_from_webpages.rb
@@ -1,5 +1,5 @@
-class RemoveUserIdFromWebpages < ActiveRecord::Migration
-  def change
-    remove_column :webpages, :user_id, :integer
-  end
-end
+# class RemoveUserIdFromWebpages < ActiveRecord::Migration
+#   def change
+#     remove_column :webpages, :user_id, :integer
+#   end
+# end
