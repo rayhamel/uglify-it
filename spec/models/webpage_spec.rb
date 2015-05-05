fe0fdaa4e0980c29f012f53bac6934631dd1fba0
Ray Hamel <rayghamel@gmail.com>
2015-05-05 00:12:49 -0400

    unit tests for webpages

--- spec/models/webpage_spec.rb
+++ spec/models/webpage_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Webpage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_length_of :title }
+  it { should validate_length_of :url }
+  it { should validate_presence_of :url }
+
+  it { should have_valid(:title).when('A Website') }
+  it { should have_valid(:url).when('example.com') }
+
+  it { should_not have_valid(:title).when('aa') }
+  it { should_not have_valid(:title).when('a' * 256) }
+  it { should_not have_valid(:url).when('') }
+  it { should_not have_valid(:url).when('www') }
+  it { should_not have_valid(:url).when('w') * 512 }
 end
