3dafa1e0e12560cadeb99ae05d8558bbb57e0fa4
Ray Hamel <rayghamel@gmail.com>
2021-03-29 11:16:30 -0400

    Fix errors caused by upgrading dependencies

--- db/migrate/20150415210428_add_slug_to_webpages.rb
+++ db/migrate/20150415210428_add_slug_to_webpages.rb
@@ -1,4 +1,4 @@
-class AddSlugToWebpages < ActiveRecord::Migration
+class AddSlugToWebpages < ActiveRecord::Migration[4.2]
   def change
     add_column :webpages, :slug, :string, unique: true
   end
